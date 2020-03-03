218330776cedffba96e37c6531d1a719edfbf36d
Gurvinder-web <61724859+Gurvinder-web@users.noreply.github.com>
2020-03-03 13:25:31 +0530

    Update entrypoint.sh
    Modifies

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,29 +1,27 @@
 #!/bin/bash
 
-if [ "$color" == "black" ]
+if  [  "$color"  ==  "black"  ]
+
+then
+        cp  /opt/ashu.html   /var/www/html/index.html
+        httpd  -DFOREGROUND
+
+elif  [   "$color"  ==  "red"    ]
+
+then
+        cp   /opt/red.html   /var/www/html/index.html
+        httpd  -DFOREGROUND
+
+elif  [   "$color"  ==   "green"     ]
 
 then
 
-	cp /opt/guri.html /var/www.html/index.html
-	httpd -DFOREGROUND
-
-elif [ "$color" == "red" ]
-
-then 
-
-	cp /opt/red.html /var/www.html/index.html
-	httpd -DFOREGROUND
-
-elif [ "$color" == "green" ]
-
-then
-
-	cp /opt/green.html /var/www.html/index.html
-        httpd -DFOREGROUND
+        cp   /opt/green.html   /var/www/html/index.html
+        httpd  -DFOREGROUND
 
 else
+        echo  "Sorry no color found  "   >/var/www/html/index.html
+        httpd  -DFOREGROUND
 
-	echo "sorry no Color found" >/var/www.html/index.html
-	httpd -DFOREGROUND	
 
-fi	
+fi
